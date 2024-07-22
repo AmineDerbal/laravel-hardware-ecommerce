85bd68435bab36ed9acb69de71892cad601d2181
Amine derbal <aminedoran31@gmail.com>
2024-07-22 16:59:45 +0100

    Setup home view

---
+++ resources/js/pages/home/HomeView.vue
@@ -0,0 +1,13 @@
+<script>
+import LayoutView from '../../components/layouts/LayoutView.vue';
+
+export default {
+  components: {
+    LayoutView,
+  },
+};
+</script>
+
+<template>
+  <LayoutView> </LayoutView>
+</template>
