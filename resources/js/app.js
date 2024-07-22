b05c3b9d97348cd971ea72dd2edbacb9680d2ad2
Amine derbal <aminedoran31@gmail.com>
2024-07-22 16:56:18 +0100

    Setup vue display

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
-import './bootstrap';
+import { createApp } from 'vue';
+import AOS from 'aos';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import BootstrapVueNext from 'bootstrap-vue-next';
+import vClickOutside from 'click-outside-vue3';
+import VueFeather from 'vue-feather';
+import router from './router';
+import App from './App.vue';
+
+import './assets/scss/config/minimal/app.scss';
+import './assets/scss/mermaid.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+const pinia = createPinia().use(piniaPluginPersistedstate);
+
+AOS.init({
+  easing: 'ease-out-back',
+  duration: 1000,
+});
+
+createApp(App)
+  .use(pinia)
+  .use(router)
+  .use(BootstrapVueNext)
+  .use(vClickOutside)
+  .component(VueFeather.type, VueFeather)
+  .mount('#app');
