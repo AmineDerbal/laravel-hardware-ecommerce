7f0ca67e55c32e573c62426f18b8a7dbae9c9071
Amine derbal <aminedoran31@gmail.com>
2024-07-23 12:51:56 +0100

    Add products in sidebar

--- resources/js/components/layouts/menu.vue
+++ resources/js/components/layouts/menu.vue
@@ -1,9 +1,10 @@
 <script>
-import { HomeIcon } from '@zhuowenli/vue-feather-icons';
+import { HomeIcon, BoxIcon } from '@zhuowenli/vue-feather-icons';
 
 export default {
   components: {
     HomeIcon,
+    BoxIcon,
   },
 
   data() {
@@ -36,6 +37,37 @@ export default {
           />
           <span data-key="Tableau de bord">Dashboard</span>
         </router-link>
+      </li>
+      <li class="nav-item">
+        <a
+          class="nav-link menu-link"
+          href="#sidebarProducts"
+          data-bs-toggle="collapse"
+          role="button"
+          aria-expanded="false"
+          aria-controls="sidebarProducts"
+        >
+          <BoxIcon
+            width="24"
+            height="24"
+          />
+          <span data-key="product">Products</span>
+        </a>
+        <div
+          class="collapse menu-dropdown"
+          id="sidebarProducts"
+        >
+          <ul class="nav nav-sm flex-column">
+            <li class="nav-item">
+              <router-link
+                to="/products"
+                class="nav-link custom-abc"
+                data-key="products"
+                >All Products</router-link
+              >
+            </li>
+          </ul>
+        </div>
       </li>
     </ul>
   </BContainer>
