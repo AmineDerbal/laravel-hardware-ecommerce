c1cf5ea2b9ba80eacbfab64e7ea3bc61a442e546
Amine derbal <aminedoran31@gmail.com>
2024-07-25 15:12:31 +0100

    Update global axios config

--- resources/js/utils/axiosUtils.js
+++ resources/js/utils/axiosUtils.js
@@ -1,9 +1,39 @@
 import axios from 'axios';
 
+import { useUserStore } from '@/state';
+
 const configureAxios = () => {
   axios.defaults.headers.common['Accept'] = 'application/json';
   axios.defaults.headers.common['Content-Type'] = 'application/json';
   axios.defaults.withCredentials = true;
+
+  axios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      if (error.response) {
+        console.log(error.response);
+        const statusCode = error.response.status;
+        if (statusCode === 401) {
+          const userStore = useUserStore();
+          await userStore.logoutUser();
+
+          window.location.href = '/login';
+          return;
+        }
+        if (statusCode === 404) {
+          window.location.href = '/404';
+          return;
+        }
+      }
+
+      return Promise.reject(error);
+    },
+  );
+  return axios;
 };
 
-export { configureAxios };
+const configuredAxios = configureAxios();
+
+export default configuredAxios;
