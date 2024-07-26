8eb462cdd9eb4f696829f6edd55bce7543e6d72c
Amine derbal <aminedoran31@gmail.com>
2024-07-26 15:03:20 +0100

    Setup Toastification

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import BootstrapVueNext from 'bootstrap-vue-next';
 import vClickOutside from 'click-outside-vue3';
 import VueFeather from 'vue-feather';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import router from './router';
 import App from './App.vue';
 
@@ -19,10 +21,21 @@ AOS.init({
   duration: 1000,
 });
 
+const options = {
+  // You can set your default options here
+  position: 'top-right',
+  timeout: 5000,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  draggablePercent: 0.6,
+};
+
 createApp(App)
   .use(pinia)
   .use(router)
   .use(BootstrapVueNext)
   .use(vClickOutside)
+  .use(Toast, options)
   .component(VueFeather.type, VueFeather)
   .mount('#app');
