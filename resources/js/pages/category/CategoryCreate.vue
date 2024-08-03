b25d99c9f223f0eab959b6b1696680af0a314e8b
Amine derbal <aminedoran31@gmail.com>
2024-08-03 13:27:35 +0100

    refactor

--- resources/js/pages/category/CategoryCreate.vue
+++ resources/js/pages/category/CategoryCreate.vue
@@ -1,12 +1,26 @@
+<template>
+  <LayoutView>
+    <LoaderView v-if="isLoading" />
+    <CategoryForm
+      v-model:category="category"
+      :errors="errors"
+      :submitForm="submitForm"
+      :selectChoicesCategories="selectChoicesCategories"
+      v-else
+    />
+  </LayoutView>
+</template>
+
 <script>
-import { onBeforeMount, computed, ref } from 'vue';
+import { onBeforeMount, computed } from 'vue';
 import { useRouter } from 'vue-router';
 import { useToast } from 'vue-toastification';
 import { LayoutView, LoaderView } from '@/components';
 import { useCategoryStore } from '@/state';
+import CategoryForm from '@/components/category/CategoryForm.vue';
 
 export default {
-  components: { LayoutView, LoaderView },
+  components: { LayoutView, LoaderView, CategoryForm },
 
   setup() {
     const toast = useToast();
@@ -15,9 +29,9 @@ export default {
     const errors = computed(() => store.errors);
     const selectChoicesCategories = computed(() => store.selectCategories);
     const isLoading = computed(() => store.isLoading);
-    const category = ref({
-      name: null,
-      parent_id: null,
+    const category = computed({
+      get: () => store.category,
+      set: (value) => (store.category = value),
     });
 
     const capitalizeFirstLetter = (str) => {
@@ -51,94 +65,3 @@ export default {
   },
 };
 </script>
-
-<template>
-  <LayoutView>
-    <LoaderView v-if="isLoading" />
-    <BForm
-      class="needs-validation"
-      v-else
-    >
-      <BCardHeader>
-        <h5 class="card-title mb-2">Fill Category Details</h5>
-      </BCardHeader>
-      <BCardBody class="mt-3">
-        <BRow>
-          <BCol
-            lg="6"
-            class="mx-auto"
-          >
-            <div class="mb-3">
-              <label
-                for="firstNameinput"
-                class="form-label"
-                >name <span class="text-danger">*</span></label
-              >
-              <BFormInput
-                type="text"
-                class="form-control"
-                v-model="category.name"
-                :placeholder="name"
-                id="firstNameinput"
-              />
-              <span
-                class="text-danger"
-                v-if="errors.name && errors.name[0]"
-                >{{ errors.name[0] }}</span
-              >
-            </div>
-          </BCol>
-        </BRow>
-        <BRow>
-          <BCol
-            lg="6"
-            class="mx-auto"
-          >
-            <label
-              for="firstNameinput"
-              class="form-label"
-              >parent categorie</label
-            >
-            <BFormSelect
-              v-model="category.parent_id"
-              class="mb-3"
-              aria-label="Default select example"
-            >
-              <BFormSelectOption :value="null"
-                >Select Categorie</BFormSelectOption
-              >
-              <BFormSelectOption
-                v-for="category in selectChoicesCategories"
-                :key="category.id"
-                :value="category.id"
-                >{{ category.name }}</BFormSelectOption
-              >
-            </BFormSelect>
-            <span
-              class="text-danger"
-              v-if="errors.parent_id"
-              >{{ errors.parent_id[0] }}</span
-            >
-          </BCol>
-        </BRow>
-        <Brow>
-          <BCol
-            lg="6"
-            class="mx-auto"
-          >
-            <div class="text-end">
-              <BButton
-                type="submit"
-                class="btn-animation waves-effect waves-light"
-                @click="submitForm"
-                :variant="'primary'"
-              >
-                Save
-              </BButton>
-            </div>
-          </BCol>
-        </Brow>
-      </BCardBody>
-    </BForm>
-  </LayoutView>
-</template>
