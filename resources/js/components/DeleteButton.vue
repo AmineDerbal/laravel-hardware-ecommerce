9c668c3fc6a8c49b32b2ec226c7bad5cdd9134a7
Amine derbal <aminedoran31@gmail.com>
2024-08-03 16:34:20 +0100

    Add delete and edit button

---
+++ resources/js/components/DeleteButton.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    item: {
+      type: String,
+      required: true,
+    },
+    handleDelete: {
+      type: Function,
+      required: false,
+    },
+  },
+};
+</script>
+
+<template>
+  <li
+    class="list-inline-item"
+    data-bs-toggle="tooltip"
+    data-bs-trigger="hover"
+    data-bs-placement="top"
+    title="delete"
+  >
+    <BLink
+      class="text-danger d-inline-block remove-item-btn"
+      @click="handleDelete(id)"
+    >
+      <i class="ri-delete-bin-5-fill fs-16"></i>
+    </BLink>
+  </li>
+</template>
---
+++ resources/js/components/EditButton.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<template>
+  <li
+    class="list-inline-item"
+    v-b-tooltip.hover
+    title="edit"
+  >
+    <router-link
+      :to="{ name: `${item}-edit`, params: { id } }"
+      class="text-primary d-inline-block"
+    >
+      <i class="ri-eye-fill fs-16"></i>
+    </router-link>
+  </li>
+</template>
