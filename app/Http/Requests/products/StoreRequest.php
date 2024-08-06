7af1315bfb1c596f30ed8c2001f8b1985a3253ac
Amine derbal <aminedoran31@gmail.com>
2024-08-06 20:29:01 +0100

    Add validation rules for store product

---
+++ app/Http/Requests/products/StoreRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\products;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+
+        return [
+            'name' => 'required|string|max:255|unique:products',
+            'category_id' => 'required|exists:categories,id',
+            'description' => 'nullable|string|max:255',
+            'price' => 'required|numeric|gt:0',
+            'stock' => 'required|integer|min:0',
+            'thumbnail' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'images' => 'nullable|array',
+            'images.*' => 'image',
+        ];
+    }
+}
