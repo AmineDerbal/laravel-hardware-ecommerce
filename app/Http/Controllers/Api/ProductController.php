08a6a27571d42206e550b724807efc859cc39043
Amine derbal <aminedoran31@gmail.com>
2024-08-07 14:51:28 +0100

    Add store method for products

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductImage;
+use App\Http\Requests\products\StoreRequest;
+
+class ProductController extends Controller
+{
+    public function store(StoreRequest $request)
+    {
+
+
+        $data = $request->validated();
+
+
+        $data['code'] = idGenerate('products', 'PROD-');
+
+
+        $image = $request->file('thumbnail');
+        $path = public_path('uploads/products');
+
+        try {
+            $image_path = uploadImage($image, $path);
+            if(!$image_path) {
+                return response()->json(['message' => 'Error in image path'], 500);
+            }
+
+            $data['image'] = $image_path;
+            $product = Product::create($data);
+
+            if(!$product) {
+                unlink($image_path);
+                return response()->json(['message' => 'Error creating product'], 500);
+            }
+
+            if($request->hasFile('images')) {
+
+                $images = $request->file('images');
+                foreach ($images as $image) {
+
+                    $image_path = uploadImage($image, $path);
+                    ProductImage::create([
+                        'product_id' => $product->id,
+                        'image' => $image_path
+                    ]);
+                }
+            }
+
+        } catch (\Throwable $th) {
+
+            return response()->json(['message' => 'Error creating product'], 500);
+        }
+
+
+        return response()->json([
+            'message' => 'Product Added successfully'
+        ]);
+
+    }
+}
