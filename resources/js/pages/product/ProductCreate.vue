e204e54c30d73302f7d502f6a1c3ba919cc22a1e
Amine derbal <aminedoran31@gmail.com>
2024-08-07 14:55:55 +0100

    Update submit form

--- resources/js/pages/product/ProductCreate.vue
+++ resources/js/pages/product/ProductCreate.vue
@@ -4,12 +4,15 @@
       v-model:product="product"
       :errors="errors"
       :selectCategories="selectCategories"
+      :submitForm="submitForm"
     />
   </LayoutView>
 </template>
 
 <script>
 import { computed, onBeforeMount } from 'vue';
+import { useRouter } from 'vue-router';
+import { useToast } from 'vue-toastification';
 import { useProductStore, useCategoryStore } from '@/state';
 import { LayoutView, LoaderView, ProductForm } from '@/components';
 
@@ -18,16 +21,30 @@ export default {
 
   setup() {
     const store = useProductStore();
+    const router = useRouter();
+    const toast = useToast();
     const categoryStore = useCategoryStore();
     const product = computed(() => store.product);
     const errors = computed(() => store.errors);
     const selectCategories = computed(() => categoryStore.selectCategories);
 
+    const submitForm = async () => {
+      const response = await store.addProduct();
+
+      if (response.status === 200 || response.status === 201) {
+        toast.success(response.data.message, { timeout: 2000 });
+        store.clearProduct();
+        router.push({ name: 'product-list' });
+      } else {
+        toast.error('Failed to create product');
+      }
+    };
+
     onBeforeMount(async () => {
       await categoryStore.getSelectCategories();
     });
 
-    return { product, errors, selectCategories };
+    return { product, errors, selectCategories, submitForm };
   },
 };
 </script>
