a6f3bee2d184ab8d6b992e86c709407bc018a276
Amine derbal <aminedoran31@gmail.com>
2024-08-10 14:07:58 +0100

    Add show button

---
+++ resources/js/components/Buttons/ShowButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <li
+    class="list-inline-item"
+    v-b-tooltip.hover
+    title="show"
+  >
+    <router-link
+      :to="{ name: `${item}-show`, params: { id } }"
+      class="text-success d-inline-block"
+    >
+      <i class="ri-eye-fill fs-16"></i>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: Number,
+      required: true,
+    },
+    item: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,7 +1,8 @@
 import LayoutView from './layouts/LayoutView.vue';
 import LoaderView from './LoaderView.vue';
-import DeleteButton from './DeleteButton.vue';
-import EditButton from './EditButton.vue';
+import DeleteButton from './Buttons/DeleteButton.vue';
+import EditButton from './Buttons/EditButton.vue';
+import ShowButton from './Buttons/ShowButton.vue';
 import Table from './table/Table.vue';
 import CategoryForm from './category/CategoryForm.vue';
 import ProductForm from './product/ProductForm.vue';
@@ -11,6 +12,7 @@ export {
   LoaderView,
   DeleteButton,
   EditButton,
+  ShowButton,
   Table,
   CategoryForm,
   ProductForm,
