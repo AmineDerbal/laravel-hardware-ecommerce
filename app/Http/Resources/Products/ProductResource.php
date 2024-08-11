92916a342ea3fe25c0826ac879587bf50122ff75
Amine derbal <aminedoran31@gmail.com>
2024-08-11 13:49:37 +0100

    Add product resource

---
+++ app/Http/Resources/Products/ProductResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\Products;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\ProductImages\AllProductImagesResource;
+use App\Http\Resources\Category\CategoryParentResource;
+
+class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'code' => $this->code,
+            'price' => $this->price,
+            'stock' => $this->stock,
+            'image_url' => $this->image_url,
+            'category' => new CategoryParentResource($this->category),
+            'images' => AllProductImagesResource::collection($this->images),
+        ];
+    }
+}
