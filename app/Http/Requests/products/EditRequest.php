a10d8a406b983dd117d833f202df8254acdd7cab
Amine derbal <aminedoran31@gmail.com>
2024-08-12 14:53:38 +0100

    Add edit validations

---
+++ app/Http/Requests/products/EditRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\products;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Product;
+
+class EditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $product = Product::findOrFail($this->id);
+        return [
+            'id' => 'required|exists:products,id',
+            'name' => 'nullable|string|max:255|unique:products,name,' . $product->id,
+            'category_id' => 'nullable|exists:categories,id',
+            'description' => 'nullable|string|max:255',
+            'price' => 'nullable|numeric|gt:0',
+            'stock' => 'nullable|integer|min:0',
+        ];
+    }
+}
