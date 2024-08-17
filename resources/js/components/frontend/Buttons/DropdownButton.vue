b48b40ee7acaf36487198f2eef2f2d8c74ad7d1b
Amine derbal <aminedoran31@gmail.com>
2024-08-17 21:09:34 +0100

    Fix dropdown behaviour

---
+++ resources/js/components/frontend/Buttons/DropdownButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="dropdown">
+    <button
+      class="btn dropdown-toggle text-white"
+      type="button"
+      id="dropdownMenuButton1"
+      data-bs-toggle="dropdown"
+      aria-expanded="false"
+    >
+      {{ item.name }}
+    </button>
+    <ul
+      class="dropdown-menu mt-3"
+      aria-labelledby="dropdownMenuButton1"
+    >
+      <div v-for="child in item.children">
+        <li v-if="child.children.length <= 0">
+          <a
+            class="dropdown-item"
+            href="#"
+          >
+            {{ child.name }}
+          </a>
+        </li>
+        <DropendButton
+          v-else
+          :item="child"
+        />
+      </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+import DropendButton from './DropendButton.vue';
+export default {
+  components: {
+    DropendButton,
+  },
+  name: 'DropdownButton',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
