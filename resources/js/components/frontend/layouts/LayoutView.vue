5a4b5db7d497e3b32fbb6750fb58c56ce27584d5
Amine derbal <aminedoran31@gmail.com>
2024-08-18 15:40:52 +0100

    Add frontend layout

---
+++ resources/js/components/frontend/layouts/LayoutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <headerView />
+
+  <div class="main-content">
+    <div class="page-content">
+      <!-- Start Content-->
+      <BContainer fluid>
+        <slot />
+      </BContainer>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderView from './HeaderView.vue';
+
+export default {
+  components: { HeaderView },
+};
+</script>
