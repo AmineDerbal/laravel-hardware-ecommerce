ae095fa5cf8325b8b1696544cc6030b14feda624
Amine derbal <aminedoran31@gmail.com>
2024-08-18 15:37:39 +0100

    Add slug to data sent

--- app/Http/Resources/Category/CategoryChildrenResource.php
+++ app/Http/Resources/Category/CategoryChildrenResource.php
@@ -17,6 +17,7 @@ class CategoryChildrenResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'slug' => $this->slug,
             'children' => CategoryChildrenResource::collection($this->children),
         ];
     }
