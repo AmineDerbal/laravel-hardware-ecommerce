258210617bf74a6ffc73e3701624e4065a19ea99
Amine derbal <aminedoran31@gmail.com>
2024-08-18 15:43:35 +0100

    Add mobile menu

---
+++ resources/js/components/frontend/layouts/MobileMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="d-block d-lg-none w-100 h-100 top-0 left-0 mobile-menu-modal position-fixed overflow-hidden"
+  ></div>
+</template>
+
+<script>
+export default {
+  name: 'MobileMenu',
+  props: {
+    items: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.mobile-menu-modal {
+  background-color: rgb(0, 0, 0); /* Fallback color */
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 10;
+}
+</style>
