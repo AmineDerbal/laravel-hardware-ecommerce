5d912fba0e2b0e59add25b65634b563a4b91f515
Amine derbal <aminedoran31@gmail.com>
2024-08-24 14:18:13 +0100

    Add categoy children path helpers function

--- app/Helpers/CodeUtility.php
+++ app/Helpers/CodeUtility.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Haruncpi\LaravelIdGenerator\IdGenerator;
 use Illuminate\Support\Facades\File;
 
@@ -24,5 +25,37 @@ if(!function_exists('checkFolderExists')) {
         if (!File::exists($path)) {
             File::makeDirectory($path, 0755, true, true);
         }
+    }
+}
+
+if(!function_exists('getChildrenRecursive')) {
+    function getChildrenRecursive(Category $category, array &$childrenArray): void
+    {
+
+        foreach ($category->children as $child) {
+            $childrenArray[] = [
+                'name' => $child->name,
+                'slug' => $child->slug
+            ];
+            if ($child->children->isNotEmpty()) {
+                getChildrenRecursive($child, $childrenArray);
+            }
+        }
+
+    }
+}
+
+
+
+if(!function_exists('buildCategoryChildrenPath')) {
+    function buildCategoryChildrenPath(Category $category): array
+    {
+        $childrenArray = [[
+            'name' => $category->name,
+            'slug' => $category->slug
+        ]];
+        getChildrenRecursive($category, $childrenArray);
+
+        return $childrenArray;
     }
 }
