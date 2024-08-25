332645bcd206f6dca21ef9b8cf10bed37ffb75c0
Amine derbal <aminedoran31@gmail.com>
2024-08-25 20:44:21 +0100

    Fix links click

--- resources/js/components/frontend/Buttons/DropdownButton.vue
+++ resources/js/components/frontend/Buttons/DropdownButton.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="dropdown">
     <button
-      class="btn dropdown-toggle text-white"
+      class="btn dropdown-toggle text-white py-2"
       type="button"
       id="dropdownMenuButton1"
-      data-bs-toggle="dropdown"
       aria-expanded="false"
+      @mouseenter="toggleDropdown"
+      @mouseleave="closeDropdown"
+      @click.stop="toggleDropdown"
+      @click="goToCategoryProductPage(item.slug)"
     >
       {{ item.name }}
     </button>
     <ul
-      class="dropdown-menu mt-3"
+      class="dropdown-menu"
       aria-labelledby="dropdownMenuButton1"
+      :class="{ show: isDropdownVisible }"
+      @mouseenter="toggleDropdown"
+      @mouseleave="closeDropdown"
     >
-      <div v-for="child in item.children">
+      <div
+        v-for="child in item.children"
+        :key="child.id"
+      >
         <li v-if="child.children.length <= 0">
           <router-link
             class="dropdown-item"
@@ -47,5 +56,36 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      isDropdownVisible: false,
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isDropdownVisible = true;
+    },
+    closeDropdown() {
+      this.isDropdownVisible = false;
+    },
+
+    goToCategoryProductPage(slug) {
+      this.$router.push({ name: 'category-products', params: { slug } });
+    },
+  },
 };
 </script>
+
+<style scoped>
+.dropdown-menu {
+  display: block;
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.2s ease;
+}
+
+.dropdown-menu.show {
+  visibility: visible;
+  opacity: 1;
+}
+</style>
--- resources/js/components/frontend/Buttons/DropendButton.vue
+++ resources/js/components/frontend/Buttons/DropendButton.vue
@@ -1,20 +1,26 @@
 <template>
   <div
     class="dropend"
-    @click.stop="toggleDropdown"
+    @mouseenter="toggleDropend"
+    @mouseleave="closeDropend"
   >
     <button
       class="btn dropdown-item dropdown-toggle menu-right-icon"
       type="button"
       id="dropendMenuButton1"
-      data-bs-toggle="dropdown"
       aria-expanded="false"
+      @mouseenter="toggleDropend"
+      @mouseleave="closeDropend"
+      @click="goToCategoryProductPage(item.slug)"
     >
       {{ item.name }}
     </button>
     <ul
       class="dropdown-menu"
+      :class="{ show: isDropdownVisible }"
       aria-labelledby="dropendMenuButton1"
+      @mouseenter="toggleDropend"
+      @mouseleave="closeDropend"
     >
       <li v-for="child in item.children">
         <router-link
@@ -40,11 +46,49 @@ export default {
       required: true,
     },
   },
+  data() {
+    return {
+      isDropdownVisible: false,
+    };
+  },
+  methods: {
+    toggleDropend() {
+      console.log('start');
+      this.isDropdownVisible = true;
+      console.log(this.isDropdownVisible);
+    },
+    closeDropend() {
+      this.isDropdownVisible = false;
+      console.log(this.isDropdownVisible);
+    },
+    goToCategoryProductPage(slug) {
+      this.$router.push({ name: 'category-products', params: { slug } });
+    },
+  },
 };
 </script>
 
 <style scoped>
 .menu-right-icon::after {
   content: '\f035f';
+}
+.dropdown-menu {
+  position: absolute;
+  top: 0;
+  left: 100%;
+  display: block;
+  visibility: hidden;
+  opacity: 0;
+  transition: opacity 0.2s ease;
+  transform: translateY(0);
+}
+
+.dropdown-menu.show {
+  visibility: visible;
+  opacity: 1;
+}
+
+.dropend .dropdown-menu {
+  left: 100%;
 }
 </style>
