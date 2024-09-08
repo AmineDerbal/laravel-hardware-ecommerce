3c611ecd62a11a6977d734b87dcb274e4cc7e453
Amine derbal <aminedoran31@gmail.com>
2024-09-08 13:50:49 +0100

    Add modal to exports

--- resources/js/components/index.js
+++ resources/js/components/index.js
@@ -1,3 +1,4 @@
+// backend
 import AdminLayoutView from './backend/layouts/LayoutView.vue';
 import LoaderView from './LoaderView.vue';
 import AdminDeleteButton from './backend/Buttons/DeleteButton.vue';
@@ -6,13 +7,18 @@ import AdminShowButton from './backend/Buttons/ShowButton.vue';
 import AdminTable from './backend/table/Table.vue';
 import AdminCategoryForm from './backend/category/CategoryForm.vue';
 import AdminProductForm from './backend/product/ProductForm.vue';
+import ConfirmAdminModalAction from './backend/modals/ConfirmActionModal.vue';
 
+// frontend
 import LayoutView from './frontend/layouts/LayoutView.vue';
 import HomeContent from './frontend/home/HomeContent.vue';
 import ProductsGrid from './frontend/products/LatestList.vue';
+
+// utils
 import PaginationUtils from './utils/PaginationUtils.vue';
 
 export {
+  // backend
   AdminLayoutView,
   LoaderView,
   AdminDeleteButton,
@@ -21,8 +27,13 @@ export {
   AdminTable,
   AdminCategoryForm,
   AdminProductForm,
+  ConfirmAdminModalAction,
+
+  // frontend
   LayoutView,
   HomeContent,
   ProductsGrid,
+
+  // utils
   PaginationUtils,
 };
