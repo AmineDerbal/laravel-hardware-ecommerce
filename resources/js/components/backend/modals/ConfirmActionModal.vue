40b52702fb0c05b3d304e3ab3b98f8ce56f1bdfd
Amine derbal <aminedoran31@gmail.com>
2024-09-08 13:25:26 +0100

    Add confirm action modal

---
+++ resources/js/components/backend/modals/ConfirmActionModal.vue
@@ -0,0 +1,77 @@
+<template>
+  <BModal
+    title="Confirm Action"
+    class="v-modal-custom"
+    v-model="show"
+    @hide="closeShowModal"
+    :key="show"
+    hide-footer
+  >
+    <BRow class="g-3">
+      <BCol xxl="12">
+        <p class="text-muted">Are you sure you want to perform this action?</p>
+      </BCol>
+      <BCol lg="12">
+        <div class="hstack gap-2 justify-content-end">
+          <BButton
+            type="button"
+            variant="danger"
+            @click="closeShowModal"
+            >Cancel</BButton
+          >
+          <BButton
+            type="button"
+            variant="primary"
+            @click="onConfirm"
+            >Confirm</BButton
+          >
+        </div>
+      </BCol>
+    </BRow>
+  </BModal>
+</template>
+
+<script>
+import { computed } from 'vue';
+export default {
+  name: 'ConfirmActionModal',
+
+  props: {
+    showModal: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+
+    confirmAction: {
+      type: Function,
+      required: true,
+    },
+  },
+
+  setup(props, { emit }) {
+    const show = computed(() => props.showModal);
+
+    const closeShowModal = () => {
+      console.log('closed');
+      emit('update:showModal', false);
+      emit('update:confirmAction', null);
+    };
+
+    const onConfirm = async () => {
+      try {
+        await props.confirmAction();
+        closeShowModal();
+      } catch (error) {
+        return;
+      }
+    };
+
+    return {
+      show,
+      closeShowModal,
+      onConfirm,
+    };
+  },
+};
+</script>
