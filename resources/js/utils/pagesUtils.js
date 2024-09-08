8ab0197d7dc399970fc66df3ac87f64a59ed3749
Amine derbal <aminedoran31@gmail.com>
2024-09-08 13:52:08 +0100

    Add util methods

--- resources/js/utils/pagesUtils.js
+++ resources/js/utils/pagesUtils.js
@@ -2,8 +2,16 @@ const getPage = (route) => route.query.page || 1;
 
 const getRouteName = (route) => route.name;
 
+const openConfirmModal = (cancelUpdate, showConfirmModal) => {
+  cancelUpdate.value = true;
+  showConfirmModal.value = true;
+};
 const navigateToPage = (page, router, route) => {
   router.push({ name: getRouteName(route), query: { page: page } });
 };
 
-export { navigateToPage, getPage, getRouteName };
+const fetchItems = async (fetchItemsFunction, route) => {
+  await fetchItemsFunction(getPage(route));
+};
+
+export { navigateToPage, getPage, getRouteName, openConfirmModal, fetchItems };
