f843c7ec09fafd7e09574053ce79b8416c7aeac7
Amine derbal <aminedoran31@gmail.com>
2024-09-09 14:40:55 +0100

    Refactor api calls

--- resources/js/state/categoryStore.js
+++ resources/js/state/categoryStore.js
@@ -1,10 +1,10 @@
 import { defineStore } from 'pinia';
-import axios from '@/utils/axiosUtils';
+import { apiRequest, apiAction } from '@/utils/apiUtils';
 
 const useCategoryStore = defineStore({
   id: 'category',
   state: () => ({
-    categories: {},
+    categories: null,
     selectCategories: [],
     headerCategories: [],
     category: {
@@ -21,114 +21,82 @@ const useCategoryStore = defineStore({
   },
 
   actions: {
+    resetState() {
+      this.isLoading = false;
+      this.hasError = false;
+      this.errors = {};
+    },
+
     clearCategory() {
       this.category = {
         id: null,
         name: null,
         parent_id: null,
       };
-      this.errors = {};
+      this.resetState();
     },
-    async getCategories(page = 1) {
-      this.isLoading = true;
-      this.hasError = false;
-      try {
-        const response = await axios.get(`/api/categories?page=${page}`);
-        this.categories = response.data;
-        return response;
-      } catch (error) {
-        this.hasError = true;
-        return response;
-      } finally {
-        this.isLoading = false;
-      }
-    },
-    async getSelectCategories() {
-      this.isLoading = true;
-      this.hasError = false;
-      this.errors = {};
-      try {
-        const response = await axios.get('/api/categories/select');
-        this.selectCategories = response.data;
-        if (this.category.id) {
-          this.selectCategories = this.selectCategories.filter(
-            (category) => category.id !== this.category.id,
-          );
-        }
 
-        return response;
-      } catch (error) {
-        this.hasError = true;
-        this.errors = error.response.data.errors;
-        return;
-      } finally {
-        this.isLoading = false;
+    async getCategories(page = 1) {
+      const response = await apiAction(
+        () => apiRequest(`/api/categories?page=${page}`),
+        this,
+        (data) => (this.categories = data),
+      );
+
+      if (response.data.data.length < 1 && page > 1) {
+        this.getCategories(page - 1);
       }
+      return response;
+    },
+
+    async getSelectCategories() {
+      const response = await apiAction(
+        () => apiRequest('/api/categories/select'),
+        this,
+        (data) => (this.selectCategories = data),
+      );
+      if (this.category.id) {
+        this.selectCategories = this.selectCategories.filter(
+          (category) => category.id !== this.category.id,
+        );
+      }
+
+      return response;
     },
 
     async getCategory(id) {
-      this.isLoading = true;
-      this.hasError = false;
-      try {
-        const response = await axios.get(`/api/categories/${id}`);
-        this.category = response.data;
-      } catch (error) {
-        this.hasError = true;
-        return;
-      } finally {
-        this.isLoading = false;
-      }
+      return await apiAction(
+        () => apiRequest(`/api/categories/${id}`),
+        this,
+        (data) => (this.category = data),
+      );
     },
 
     async getHeaderCategories() {
-      this.hasError = false;
-      try {
-        const response = await axios.get('/api/categories/header');
-        this.headerCategories = response.data;
-        return response;
-      } catch (error) {
-        this.hasError = true;
-        return;
-      }
+      return await apiAction(
+        () => apiRequest('/api/categories/header'),
+        this,
+        (data) => (this.headerCategories = data),
+      );
     },
 
     async createCategory(category) {
-      this.isLoading = true;
-      this.hasError = false;
-      this.errors = {};
-      try {
-        let response;
-        if (category.id !== null) {
-          response = await axios.put('/api/categories/update', category);
-        } else {
-          response = await axios.post('/api/categories/store', category);
-          return response;
-        }
-
-        return response;
-      } catch (error) {
-        this.hasError = true;
-        if (error.response.status === 422) {
-          this.errors = error.response.data.errors;
-        }
-        return error.response;
-      } finally {
-        this.isLoading = false;
-      }
+      return category.id === null
+        ? await apiAction(
+            () => apiRequest('/api/categories/store', 'POST', category),
+            this,
+          )
+        : await apiAction(
+            () => apiRequest('/api/categories/update', 'PUT', category),
+            this,
+          );
     },
 
     async deleteCategory(id) {
-      this.isLoading = true;
-      this.hasError = false;
-      try {
-        const response = await axios.delete(`/api/categories/${id}`);
-        return response;
-      } catch (error) {
-        this.hasError = true;
-        return;
-      } finally {
-        this.isLoading = false;
-      }
+      return await apiAction(
+        () => apiRequest(`/api/categories/${id}`, 'DELETE'),
+        this,
+      );
     },
   },
 });
