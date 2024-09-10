7714defc2492aa5ffcc2e16580bb0e3def27faa7
Amine derbal <aminedoran31@gmail.com>
2024-09-10 15:09:25 +0100

    Fix products display

--- resources/js/pages/frontend/product/CategoryProductsList.vue
+++ resources/js/pages/frontend/product/CategoryProductsList.vue
@@ -8,41 +8,47 @@
       :key="categoryProducts"
     >
       <BRow class="mb-3 mx-0">
-        <div class="d-none d-lg-block">
-          <div class="mb-3">
-            <nav aria-label="breadcrumb">
-              <BBreadcrumb>
-                <BBreadcrumbItem>
-                  <RouterLink to="/">Home</RouterLink></BBreadcrumbItem
-                >
-                <BBreadcrumbItem
-                  v-for="category in categoryProducts.category_parent_path"
-                  :key="category.slug"
-                >
-                  <RouterLink
-                    :to="{
-                      name: 'category-products',
-                      params: { slug: category.slug },
-                    }"
-                    >{{ category.name }}</RouterLink
+        <BCol
+          class="d-none d-lg-block"
+          lg="2"
+        ></BCol>
+        <BCol lg="10">
+          <div>
+            <div class="mb-3">
+              <nav aria-label="breadcrumb">
+                <BBreadcrumb>
+                  <BBreadcrumbItem>
+                    <RouterLink to="/">Home</RouterLink></BBreadcrumbItem
                   >
-                </BBreadcrumbItem>
-                <BBreadcrumbItem active
-                  ><span class="text-primary"
-                    >Page {{ page }}</span
-                  ></BBreadcrumbItem
-                >
-              </BBreadcrumb>
-            </nav>
+                  <BBreadcrumbItem
+                    v-for="category in categoryProducts.category_parent_path"
+                    :key="category.slug"
+                  >
+                    <RouterLink
+                      :to="{
+                        name: 'category-products',
+                        params: { slug: category.slug },
+                      }"
+                      >{{ category.name }}</RouterLink
+                    >
+                  </BBreadcrumbItem>
+                  <BBreadcrumbItem active
+                    ><span class="text-primary"
+                      >Page {{ page }}</span
+                    ></BBreadcrumbItem
+                  >
+                </BBreadcrumb>
+              </nav>
+            </div>
+            <ProductsGrid :items="categoryProducts.products.data" />
+            <PaginationUtils
+              :meta="categoryProducts.products.meta"
+              :links="categoryProducts.products.links"
+              :onPageChange="onPageChange"
+              :isRed="true"
+            />
           </div>
-          <ProductsGrid :items="categoryProducts.products.data" />
-          <PaginationUtils
-            :meta="categoryProducts.products.meta"
-            :links="categoryProducts.products.links"
-            :onPageChange="onPageChange"
-            :isRed="true"
-          />
-        </div>
+        </BCol>
       </BRow>
     </div>
   </LayoutView>
