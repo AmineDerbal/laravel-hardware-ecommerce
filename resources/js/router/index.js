90b4f50160c3e3c245523a8002466edfadf1548e
Amine derbal <aminedoran31@gmail.com>
2024-09-11 13:29:55 +0100

    update errors display

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -9,9 +9,15 @@ const router = createRouter({
 
 router.beforeEach((to, from, next) => {
   const userStore = useUserStore();
-  if (to.meta.requireAuth && !userStore.user.isAuthenticated) {
+  if (
+    to.meta.requireAuth &&
+    !userStore.user.isAuthenticated &&
+    userStore.user.role !== 0
+  ) {
+    const toastMessage = 'You must be an admin to access the page';
     next({
       name: 'login',
+      query: { message: toastMessage, type: 'error' },
     });
   } else {
     next();
