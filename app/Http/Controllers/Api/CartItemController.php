e9522a595f3cca76970c489c380b429b189839cf
Amine derbal <aminedoran31@gmail.com>
2024-09-15 15:54:46 +0100

    Add store method

--- app/Http/Controllers/Api/CartItemController.php
+++ app/Http/Controllers/Api/CartItemController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\CartItem;
+use App\Models\Cart;
+use App\Http\Requests\CartItems\StoreRequest;
 use Illuminate\Http\Request;
 
 class CartItemController extends Controller
 {
-    public function store()
+    public function store(StoreRequest $request)
     {
+
+        $data = $request->validated();
+        $cart = Cart::firstOrCreate(['active' => 1,'cart_id' => $data['cart_id']], ['user_id' => $data['user_id']]);
+        $data->cart_id = $cart->id;
+
+
+        $cartItem = CartItem::create($data->toArray());
+
+
+        return $cartItem ? response()->json(['message' => 'Cart item created successfully']) : response()->json(['message' => 'Error creating cart item'], 500);
+
+
 
     }
 }
