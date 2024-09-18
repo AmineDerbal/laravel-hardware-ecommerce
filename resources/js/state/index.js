ed0befebffad9cdeece323a27109dcc368ceed91
Amine derbal <aminedoran31@gmail.com>
2024-09-18 12:21:59 +0100

    Add cart store

--- resources/js/state/index.js
+++ resources/js/state/index.js
@@ -2,5 +2,12 @@ import useLayoutStore from './layoutStore';
 import useUserStore from './userStore';
 import useCategoryStore from './categoryStore';
 import useProductStore from './productStore';
+import useCartStore from './CartStore';
 
-export { useLayoutStore, useUserStore, useCategoryStore, useProductStore };
+export {
+  useLayoutStore,
+  useUserStore,
+  useCategoryStore,
+  useProductStore,
+  useCartStore,
+};
