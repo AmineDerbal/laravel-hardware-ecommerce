8c7e9eba80dc6c61c1a167482508b9c219429d2b
Amine derbal <aminedoran31@gmail.com>
2024-09-18 12:19:19 +0100

    Add fetch users cart items method

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Resources\Users\UserCollection;
 
@@ -19,8 +18,8 @@ class UserController extends Controller
 
     public function toggleIsActiveStatus($id)
     {
-        try {
 
+        try {
             $user = User::findOrFail($id);
 
             $is_active = $user->is_active == 1 ? 0 : 1;
@@ -31,5 +30,17 @@ class UserController extends Controller
             return response()->json(['message' => 'Error updating user'], 500);
         }
 
+    }
+
+    public function fetchUserActiveCartItems($id)
+    {
+        try {
+            $user = User::with(['cartItems'])->find($id);
+
+            return response()->json($user->cartItems);
+        } catch (\Exception $e) {
+            \Log::info('Error fetching cart items: ' . $e->getMessage());
+            return response()->json(['message' => 'Error fetching cart items'], 500);
+        }
     }
 }
