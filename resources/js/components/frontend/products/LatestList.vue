d7847d6159c10f14a3f99902772fdd55489cb568
Amine derbal <aminedoran31@gmail.com>
2024-09-18 12:21:30 +0100

    Handle add to cart click method

--- resources/js/components/frontend/products/LatestList.vue
+++ resources/js/components/frontend/products/LatestList.vue
@@ -32,11 +32,25 @@
             $ {{ item.price }}
           </p>
           <div class="text-end">
-            <BLink
-              href="javascript:void(0);"
-              class="btn btn-outline-danger whb-red-bg text-uppercase fs-16 text-white w-100"
-              >Add to cart</BLink
+            <button
+              type="button"
+              class="btn btn-outline-danger btn-load whb-red-bg text-uppercase fs-16 text-white w-100"
+              @click="handleAddToCartBtnClick(item.id)"
             >
+              <span class="d-flex align-items-center justify-content-center">
+                <span
+                  class="spinner-border flex-shrink-0"
+                  role="status"
+                  v-if="isLoading[item.id]"
+                >
+                </span>
+                <span
+                  class="flex-frow-1 ms-2"
+                  v-else
+                  >Add to cart
+                </span>
+              </span>
+            </button>
           </div>
         </BCardBody>
       </BCard>
@@ -45,7 +59,11 @@
 </template>
 
 <script>
+import { computed, ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { useToast } from 'vue-toastification';
+import { useCartStore, useUserStore } from '@/state';
+
 export default {
   props: {
     items: {
@@ -56,6 +74,52 @@ export default {
 
   setup() {
     const router = useRouter();
+    const cartStore = useCartStore();
+    const userStore = useUserStore();
+    const toast = useToast();
+
+    const user = computed(() => userStore.user);
+    const isLoading = ref({});
+
+    const isUserLoggedIn = computed(() =>
+      user.value.isAuthenticated && user.value.name && user.value.email
+        ? true
+        : false,
+    );
+
+    const handleAddToCartBtnClick = async (product_id) => {
+      if (!isUserLoggedIn.value) {
+        toast.error('Please login to add product to cart');
+        return;
+      }
+
+      const { id, cart_id } = userStore.checkIfItemIsInCart(product_id) || {};
+
+      const item = {
+        user_id: user.value.id,
+        product_id,
+        quantity: 1,
+        id,
+        cart_id,
+      };
+
+      isLoading.value = { ...isLoading.value, [product_id]: true };
+      const result = await cartStore.addItemToCart(item);
+      if (result.status !== 200) {
+        isLoading.value = { ...isLoading.value, [product_id]: false };
+        toast.error(result.data.message);
+        return;
+      }
+      const response = await userStore.fetchUserActiveCartItems(user.value.id);
+      if (response.status !== 200) {
+        toast.error(response.data.message);
+        return;
+      }
+
+      toast.success(result.data.message);
+
+      isLoading.value = { ...isLoading.value, [product_id]: false };
+    };
 
     const viewProduct = (slug) => {
       router.push({ name: 'product-show', params: { slug } });
@@ -63,6 +127,9 @@ export default {
 
     return {
       viewProduct,
+      isUserLoggedIn,
+      isLoading,
+      handleAddToCartBtnClick,
     };
   },
 };
