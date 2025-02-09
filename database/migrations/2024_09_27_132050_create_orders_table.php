090b058b3048f7e8c1719ef0e549ad1d8eececfe
Amine derbal <aminedoran31@gmail.com>
2025-02-09 19:14:30 +0100

    Update order migration

--- database/migrations/2024_09_27_132050_create_orders_table.php
+++ database/migrations/2024_09_27_132050_create_orders_table.php
@@ -12,7 +12,6 @@ return new class () extends Migration {
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('order_number');
             $table->string('slug');
             $table->foreignId('user_id')->constrained();
             $table->string('status');
