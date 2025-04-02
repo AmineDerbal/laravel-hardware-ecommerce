533f18bf53d623507045b3fcc2fc8eea47aeabf4
Amine derbal <aminedoran31@gmail.com>
2025-04-02 20:14:06 +0100

    Add basic resource

---
+++ app/Http/Resources/Orders/BasicOrderResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources\Orders;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BasicOrderResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'status' => $this->status,
+            'total_amount' => $this->total_amount,
+            'shipping_fee' => $this->shipping_fee,
+            'tax' => $this->tax,
+            'created_at' => $this->created_at,
+            'user' => [
+                'id' => $this->user->id,
+                'name' => $this->user->name
+            ],
+
+
+        ];
+    }
+}
