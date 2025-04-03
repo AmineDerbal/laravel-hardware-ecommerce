212db6e4437619ed228c4f85ea0b888fa0e99b60
Amine derbal <aminedoran31@gmail.com>
2025-04-03 09:19:48 +0100

    Fix status enum

--- database/migrations/2024_09_27_132050_create_orders_table.php
+++ database/migrations/2024_09_27_132050_create_orders_table.php
@@ -14,7 +14,7 @@ return new class () extends Migration {
             $table->id();
             $table->string('code');
             $table->foreignId('user_id')->constrained();
-            $table->string('status');
+            $table->enum('status', ['processing', 'shipped', 'delivered', 'completed', 'returned'])->default('processing');
             $table->decimal('shipping_fee', 10, 2);
             $table->decimal('tax', 10, 2);
             $table->decimal('total_amount', 10, 2);
