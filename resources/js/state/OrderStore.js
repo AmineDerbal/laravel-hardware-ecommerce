d19c6b91763862598634ac480bbbfca3d96f82ca
Amine derbal <aminedoran31@gmail.com>
2025-04-03 09:21:05 +0100

    Add get orders action

--- resources/js/state/OrderStore.js
+++ resources/js/state/OrderStore.js
@@ -5,11 +5,27 @@ const useOrderStore = defineStore({
   id: 'order',
   state: () => ({
     order: {},
+    orders: null,
     isLoading: false,
     hasError: false,
   }),
+  persist: {
+    paths: ['orders'],
+  },
 
   actions: {
+    async getOrders(page = 1) {
+      const response = await apiAction(
+        () => apiRequest(`/api/admin/orders?page=${page}`),
+        this,
+        (data) => (this.orders = data),
+      );
+
+      if (response.data.data.length < 1 && page > 1) {
+        this.getOrders(page - 1);
+      }
+      return response;
+    },
     async storeOrder(order) {
       return await apiAction(
         () => apiRequest('/api/orders/store', 'POST', order),
