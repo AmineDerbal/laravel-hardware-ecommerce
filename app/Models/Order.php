c16b278ccfeec778f5eec79103f2d99c420959e3
Amine derbal <aminedoran31@gmail.com>
2025-04-04 19:55:52 +0100

    Add status label

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,6 +10,20 @@ class Order extends Model
     use HasFactory;
 
     protected $fillable = ['user_id','status','code','shipping_fee','tax','total_amount'];
+    protected $appends = ['status_label'];
+
+    public const STATUS_LABELS = [
+        0 => 'Pending',
+        1 => 'Shipped',
+        2 => 'Delivered',
+        3 => 'Completed',
+        4 => 'Cancelled',
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        return self::STATUS_LABELS[$this->status];
+    }
 
     public function user()
     {
